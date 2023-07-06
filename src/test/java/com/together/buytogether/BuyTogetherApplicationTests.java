169ef2d8c0ce56ad8f3aa4029b5aafebb2b4acb2
Kim sun wook <kimsw9603@gmail.com>
2023-07-06 22:07:34 +0900

    프로젝트 세팅

---
+++ src/test/java/com/together/buytogether/BuyTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.together.buytogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
