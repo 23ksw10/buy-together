f5fcbdd013414c0a68281ee0cda190394c8136a2
Kim sun wook <kimsw9603@gmail.com>
2023-09-18 03:14:42 +0900

    10. Request에 Valid 추가

--- src/main/java/com/together/buytogether/member/feature/RegisterMember.java
+++ src/main/java/com/together/buytogether/member/feature/RegisterMember.java
@@ -4,6 +4,9 @@ import com.together.buytogether.member.domain.Address;
 import com.together.buytogether.member.domain.Member;
 import com.together.buytogether.member.domain.MemberRepository;
 import com.together.buytogether.member.domain.SEX;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,19 +24,26 @@ public class RegisterMember {
 
     @PostMapping("/members")
     @ResponseStatus(HttpStatus.CREATED)
-    public void request(@RequestBody Request request) {
+    public void request(@RequestBody @Valid Request request) {
         // request에서 필요한 값들을 꺼내서 회원 도메인을 생성하고 저장한
         Member member = request.toDomain();
         memberRepository.save(member);
     }
 
     public record Request(
+            @NotBlank(message = "이름은 필수 값입니다")
             String name,
+            @NotBlank(message = "로그인 아이디는 필수 값입니다")
             String loginId,
+            @NotBlank(message = "비밀번호는 필수 값입니다")
             String password,
+            @NotBlank(message = "전화번호는 필수 값입니다")
             String phoneNumber,
+            @NotNull(message = "성별은 필수 값입니다")
             SEX sex,
+            @NotBlank(message = "주소는 필수 값입니다")
             String address,
+            @NotBlank(message = "상세 주소는 필수 값입니다")
             String detailAddress) {
         public Request {
             Assert.hasText(name, "이름은 필수 값입니다");
