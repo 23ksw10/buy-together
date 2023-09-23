45b73502b9ad94ce815e52ac67d309c87b7bdf85
Kim sun wook <kimsw9603@gmail.com>
2023-09-24 03:15:32 +0900

    2. SessionManger와 MemberRepository 생성자 주입으로

--- src/test/java/com/together/buytogether/member/feature/SignInMemberTest.java
+++ src/test/java/com/together/buytogether/member/feature/SignInMemberTest.java
@@ -2,31 +2,31 @@ package com.together.buytogether.member.feature;
 
 import com.together.buytogether.common.ApiTest;
 import com.together.buytogether.common.Scenario;
-import com.together.buytogether.member.domain.Member;
 import com.together.buytogether.member.domain.MemberRepository;
+import com.together.buytogether.member.domain.SessionConst;
+import com.together.buytogether.member.domain.SessionManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SignInMemberTest extends ApiTest {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private SessionManager sessionManager;
 
 
     @Test
     @DisplayName("로그인 성공")
     void signUpMember() {
         Scenario.registerMember().request().signInMember().request();
-
-        Member member = memberRepository.findByLoginId("loginId").stream()
-                .filter(m -> m.getPassword().equals("password"))
-                .findFirst()
-                .orElseThrow(null);
-        assertThat(member.getPassword()).isEqualTo("password");
-
+        assertThat(sessionManager.getAllSessions().get(0).getAttribute(SessionConst.LOGIN_MEMBER)).
+                isEqualTo(1L);
     }
 
 }
