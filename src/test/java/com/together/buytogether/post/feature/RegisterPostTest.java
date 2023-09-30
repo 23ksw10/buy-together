5f0c74131ea3432115a8b9d24dc54356a2b31f1f
Kim sun wook <kimsw9603@gmail.com>
2023-09-30 20:20:56 +0900

    7. Scenario 방식으로 변경

--- src/test/java/com/together/buytogether/post/feature/RegisterPostTest.java
+++ src/test/java/com/together/buytogether/post/feature/RegisterPostTest.java
@@ -4,13 +4,9 @@ import com.together.buytogether.common.ApiTest;
 import com.together.buytogether.common.Scenario;
 import com.together.buytogether.member.domain.SessionManager;
 import com.together.buytogether.post.domain.PostRepository;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-
-import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,24 +20,10 @@ public class RegisterPostTest extends ApiTest {
     @Test
     @DisplayName("게시글 등록")
     void registerPost() {
-        Scenario.registerMember().request().signInMember().request();
-        Long memberId = 1L;
-        String title = "title";
-        String content = "content";
-        LocalDateTime expiredAt = LocalDateTime.now().plusHours(10);
-        RegisterPost.Request request = new RegisterPost.Request(
-                title,
-                content,
-                expiredAt
-        );
-        RestAssured.given().log().all()
-                .cookie(sessionManager.getAllSessions().get(0).getId())
-                .contentType("application/json")
-                .body(request)
-                .when()
-                .post("/posts")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+        Scenario.registerMember().request()
+                .signInMember().request()
+                .registerPost().cookieName(sessionManager.getAllSessions().get(0).getId()).request();
+
         assertThat(postRepository.findAll()).hasSize(1);
 
     }
