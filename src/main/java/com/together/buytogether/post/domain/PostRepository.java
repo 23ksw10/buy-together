f66969d73c460119f1b603e28d73f76af82cde04
Kim sun wook <kimsw9603@gmail.com>
2023-10-08 23:24:49 +0900

    9. Jpa Repository 변환

--- src/main/java/com/together/buytogether/post/domain/PostRepository.java
+++ src/main/java/com/together/buytogether/post/domain/PostRepository.java
@@ -1,23 +1,9 @@
 package com.together.buytogether.post.domain;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Component
-public class PostRepository {
-    private final Map<Long, Post> posts = new HashMap<>();
-    private Long id = 1L;
+public interface PostRepository extends JpaRepository<Post, Long> {
 
-    public void save(Post post) {
-        post.assignId(id++);
-        posts.put(post.getId(), post);
-    }
-
-    public List<Post> findAll() {
-        return new ArrayList<>(posts.values());
-    }
 }
