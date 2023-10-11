ad05a22675c0785639e1368ec56f49f4cb984a02
Kim sun wook <kimsw9603@gmail.com>
2023-10-11 21:41:09 +0900

    5. confirm UpdatePostTest 구현

--- src/test/java/com/together/buytogether/post/feature/UpdatePostTest.java
+++ src/test/java/com/together/buytogether/post/feature/UpdatePostTest.java
@@ -2,23 +2,33 @@ package com.together.buytogether.post.feature;
 
 import com.together.buytogether.member.domain.MemberRepository;
 import com.together.buytogether.post.domain.Post;
+import com.together.buytogether.post.domain.PostFixture;
 import com.together.buytogether.post.domain.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UpdatePostTest {
     UpdatePost updatePost;
+    PostRepository postRepository;
 
     @BeforeEach
     void setUp() {
-        updatePost = new UpdatePost();
+        postRepository = Mockito.mock(PostRepository.class);
+        updatePost = new UpdatePost(postRepository);
     }
 
     @Test
+    @DisplayName("게시글 수정")
     void updatePost() {
+        Post post = PostFixture.aPost().build();
+        Mockito.when(postRepository.getByPostId(Mockito.anyLong())).thenReturn(post);
         Long memberId = 1L;
         Long postId = 1L;
         String newTitle = "newTitle";
@@ -30,11 +40,18 @@ public class UpdatePostTest {
         );
         updatePost.request(memberId, postId, request);
 
+        assertThat(post.getTitle()).isEqualTo(newTitle);
+        assertThat(post.getContent()).isEqualTo(newContent);
+
     }
 
     private class UpdatePost {
         MemberRepository memberRepository;
         PostRepository postRepository;
+
+        public UpdatePost(PostRepository postRepository) {
+            this.postRepository = postRepository;
+        }
 
         public void request(Long memberId, Long postId, Request request) {
             Post post = postRepository.getByPostId(postId);
