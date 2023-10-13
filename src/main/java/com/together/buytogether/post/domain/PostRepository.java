11600e622928f12f5e4be838fd10039b0a981991
Kim sun wook <kimsw9603@gmail.com>
2023-10-13 16:30:32 +0900

    3. 에러 메시지 postId도 나오게

--- src/main/java/com/together/buytogether/post/domain/PostRepository.java
+++ src/main/java/com/together/buytogether/post/domain/PostRepository.java
@@ -8,6 +8,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     default Post getByPostId(Long postId) {
         return findById(postId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 게시글입니다"));
+                () -> new IllegalArgumentException("존재하지 않는 게시글입니다. %d".formatted(postId)));
     }
 }
