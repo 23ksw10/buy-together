f992884732a20328ef1bb4c5b51bfe2fbf13f2cb
Kim sun wook <kimsw9603@gmail.com>
2023-10-14 03:40:30 +0900

    2. getByMemberId method 추가

--- src/main/java/com/together/buytogether/member/domain/MemberRepository.java
+++ src/main/java/com/together/buytogether/member/domain/MemberRepository.java
@@ -6,4 +6,10 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByLoginId(String loginId);
+
+    default Member getByMemberId(Long memberId) {
+        return findById(memberId).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 회원입니다")
+        );
+    }
 }
