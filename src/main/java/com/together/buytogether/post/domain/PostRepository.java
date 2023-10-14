d869bc0d9cd56a8aad2b8d8fa1b9df6fe19c4ae0
Kim sun wook <kimsw9603@gmail.com>
2023-10-14 15:41:52 +0900

    5. @Component 제거

--- src/main/java/com/together/buytogether/post/domain/PostRepository.java
+++ src/main/java/com/together/buytogether/post/domain/PostRepository.java
@@ -1,9 +1,8 @@
 package com.together.buytogether.post.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     default Post getByPostId(Long postId) {
