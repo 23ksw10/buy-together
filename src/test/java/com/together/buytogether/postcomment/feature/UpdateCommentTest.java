c2e62e83171631070c4ada3b73cab2ef8c254214
Kim sun wook <kimsw9603@gmail.com>
2023-10-17 03:26:03 +0900

    5. Scenario로 변경

--- src/test/java/com/together/buytogether/postcomment/feature/UpdateCommentTest.java
+++ src/test/java/com/together/buytogether/postcomment/feature/UpdateCommentTest.java
@@ -2,11 +2,8 @@ package com.together.buytogether.postcomment.feature;
 
 import com.together.buytogether.common.ApiTest;
 import com.together.buytogether.common.Scenario;
-import com.together.buytogether.member.domain.MemberRepository;
 import com.together.buytogether.member.domain.SessionManager;
-import com.together.buytogether.post.domain.PostRepository;
 import com.together.buytogether.postcomment.domain.PostCommentRepository;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class UpdateCommentTest extends ApiTest {
-    @Autowired
-    private UpdateComment updateComment;
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private PostRepository postRepository;
+
     @Autowired
     private PostCommentRepository postCommentRepository;
     @Autowired
@@ -33,25 +25,12 @@ public class UpdateCommentTest extends ApiTest {
         Scenario.registerMember().request()
                 .signInMember().request()
                 .registerPost().cookieValue(sessionManager.getAllSessions().get(0).getId()).request()
-                .registerComment().cookieValue(sessionManager.getAllSessions().get(0).getId()).request();
+                .registerComment().cookieValue(sessionManager.getAllSessions().get(0).getId()).request()
+                .updateComment().cookieValue(sessionManager.getAllSessions().get(0).getId()).request();
 
-        String content = "댓글 수정";
-        Long memberId = 1L;
-        Long postId = 1L;
-        Long commentId = 1L;
-        UpdateComment.Request request = new UpdateComment.Request(
-                content
-        );
-        RestAssured.given().log().all()
-                .cookie("JSESSIONID", sessionManager.getAllSessions().get(0).getId())
-                .contentType("application/json")
-                .body(request)
-                .when()
-                .put("/posts/{postId}/comments/{commentId}", postId, commentId)
-                .then().log().all()
-                .statusCode(200);
 
-        assertThat(postCommentRepository.getByCommentId(commentId).getContent()).isEqualTo(content);
+        assertThat(postCommentRepository.getByCommentId(1L).getContent()).isEqualTo("댓글 수정");
     }
+
 
 }
