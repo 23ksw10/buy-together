6e85833471b8d9fefe4916712fca593fdfef52e2
Kim sun wook <kimsw9603@gmail.com>
2023-10-17 17:53:02 +0900

    4. Scenario로 변경

--- src/test/java/com/together/buytogether/postcomment/feature/DeleteCommentTest.java
+++ src/test/java/com/together/buytogether/postcomment/feature/DeleteCommentTest.java
@@ -4,17 +4,14 @@ import com.together.buytogether.common.ApiTest;
 import com.together.buytogether.common.Scenario;
 import com.together.buytogether.member.domain.SessionManager;
 import com.together.buytogether.postcomment.domain.PostCommentRepository;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeleteCommentTest extends ApiTest {
-    @Autowired
-    private DeleteComment deleteComment;
+
     @Autowired
     private PostCommentRepository postCommentRepository;
     @Autowired
@@ -26,16 +23,12 @@ public class DeleteCommentTest extends ApiTest {
         Scenario.registerMember().request()
                 .signInMember().request()
                 .registerPost().cookieValue(sessionManager.getAllSessions().get(0).getId()).request()
-                .registerComment().cookieValue(sessionManager.getAllSessions().get(0).getId()).request();
+                .registerComment().cookieValue(sessionManager.getAllSessions().get(0).getId()).request()
+                .deleteComment().cookieValue(sessionManager.getAllSessions().get(0).getId()).request();
 
-        RestAssured.given().log().all()
-                .when()
-                .cookie("JSESSIONID", sessionManager.getAllSessions().get(0).getId())
-                .delete("/posts/1/comments/1")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value());
 
         assertThat(postCommentRepository.findAll()).hasSize(0);
     }
+
 
 }
