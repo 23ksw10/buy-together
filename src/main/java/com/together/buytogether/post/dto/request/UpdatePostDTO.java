c9ae4b01aaac01cea3a0edaf2f466dc296a86939
Kim sun wook <kimsw9603@gmail.com>
2023-10-17 15:32:42 +0900

    5. UpdatePostDTO 분리

---
+++ src/main/java/com/together/buytogether/post/dto/request/UpdatePostDTO.java
@@ -0,0 +1,19 @@
+package com.together.buytogether.post.dto.request;
+
+import com.together.buytogether.post.domain.PostStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+
+public record UpdatePostDTO(
+        @NotBlank(message = "글 제목은 필수입니다")
+        String title,
+        @NotBlank(message = "글 내용은 필수입니다")
+        String content,
+        @NotNull(message = "글 상태는 필수입니다")
+        PostStatus status,
+        @NotNull(message = "글 만료일은 필수입니다")
+        LocalDateTime expiredAt) {
+}
