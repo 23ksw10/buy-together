5b9fb52af35bd5b97b11a040a508498fa0430f86
Kim sun wook <kimsw9603@gmail.com>
2023-10-24 18:40:55 +0900

    1. create MemberService

--- src/main/java/com/together/buytogether/member/feature/RegisterMember.java
+++ src/main/java/com/together/buytogether/member/feature/RegisterMember.java
@@ -1,8 +1,7 @@
 package com.together.buytogether.member.feature;
 
-import com.together.buytogether.member.domain.Member;
-import com.together.buytogether.member.domain.MemberRepository;
 import com.together.buytogether.member.dto.request.RegisterMemberDTO;
+import com.together.buytogether.member.service.MemberService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,29 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterMember {
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    public RegisterMember(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public RegisterMember(MemberService memberService) {
+        this.memberService = memberService;
     }
 
     @PostMapping("/members")
     @ResponseStatus(HttpStatus.CREATED)
     public void request(@RequestBody @Valid RegisterMemberDTO registerMemberDTO) {
-        // request에서 필요한 값들을 꺼내서 회원 도메인을 생성하고 저장한다
-        // 중복 검사도
-        validateDuplicateMember(registerMemberDTO.loginId());
-        Member member = registerMemberDTO.toDomain();
-        memberRepository.save(member);
-    }
-
-    private void validateDuplicateMember(String logingId) {
-        memberRepository.findAll().stream()
-                .filter(member -> member.getLoginId().equals(logingId))
-                .findFirst()
-                .ifPresent(member -> {
-                    throw new IllegalArgumentException("이미 존재하는 회원입니다");
-                });
+        memberService.registerMember(registerMemberDTO);
     }
 
 }
