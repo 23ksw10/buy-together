8698693455aaa9bc6bdcd953862fa30ab85d4e17
Kim sun wook <kimsw9603@gmail.com>
2023-10-26 04:25:36 +0900

    2. MemberController로 전환

---
+++ src/main/java/com/together/buytogether/member/controller/MemberController.java
@@ -0,0 +1,55 @@
+package com.together.buytogether.member.controller;
+
+import com.together.buytogether.member.domain.Member;
+import com.together.buytogether.member.domain.SessionConst;
+import com.together.buytogether.member.domain.SessionManager;
+import com.together.buytogether.member.dto.request.RegisterMemberDTO;
+import com.together.buytogether.member.dto.request.SignInMemberDTO;
+import com.together.buytogether.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+    private final SessionManager sessionManager;
+
+
+    public MemberController(
+            MemberService memberService,
+            SessionManager sessionManager) {
+        this.memberService = memberService;
+        this.sessionManager = sessionManager;
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerMember(@RequestBody @Valid RegisterMemberDTO registerMemberDTO) {
+        memberService.registerMember(registerMemberDTO);
+    }
+
+    @PostMapping("/sign-in")
+    public void signInMember(
+            @RequestBody @Valid SignInMemberDTO signInMemberDTO,
+            HttpServletRequest httpServletRequest) {
+        Member logInMember = memberService.getLogInMember(signInMemberDTO.loginId(), signInMemberDTO.password());
+        HttpSession httpSession = httpServletRequest.getSession();
+        httpSession.setAttribute(SessionConst.LOGIN_MEMBER, logInMember.getMemberId());
+        String sessionId = httpSession.getId();
+        sessionManager.createSession(sessionId, httpSession);
+    }
+
+    @PostMapping("/sign-out")
+    public void signOutMember(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        sessionManager.invalidateSession(session.getId());
+    }
+}
