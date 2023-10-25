888e1b404babfb3b52e18bd9a64d87abbf9478da
Kim sun wook <kimsw9603@gmail.com>
2023-10-26 03:31:19 +0900

    6. create EnrollService

---
+++ src/main/java/com/together/buytogether/enroll/service/EnrollService.java
@@ -0,0 +1,51 @@
+package com.together.buytogether.enroll.service;
+
+import com.together.buytogether.common.service.CommonMemberService;
+import com.together.buytogether.common.service.CommonPostService;
+import com.together.buytogether.enroll.domain.Enroll;
+import com.together.buytogether.enroll.domain.EnrollRepository;
+import com.together.buytogether.member.domain.Member;
+import com.together.buytogether.post.domain.Post;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EnrollService {
+    private final CommonMemberService commonMemberService;
+    private final CommonPostService commonPostService;
+    private final EnrollRepository enrollRepository;
+
+    public EnrollService(
+            CommonMemberService commonMemberService,
+            CommonPostService commonPostService,
+            EnrollRepository enrollRepository) {
+        this.commonMemberService = commonMemberService;
+        this.commonPostService = commonPostService;
+        this.enrollRepository = enrollRepository;
+    }
+
+
+    @Transactional
+    public void joinBuying(Long memberId, Long postId) {
+        Member member = commonMemberService.getMember(memberId);
+        Post post = commonPostService.getPost(postId);
+        enrollRepository.findByMemberIdAndPostId(memberId, postId).ifPresent(enroll -> {
+            throw new IllegalArgumentException("이미 참여한 구매글입니다.");
+        });
+        Enroll enroll = new Enroll(member, post, LocalDateTime.now());
+        post.increaseJoinCount();
+        enrollRepository.save(enroll);
+    }
+
+    @Transactional
+    public void cancelBuying(Long memberId, Long postId) {
+        Post post = commonPostService.getPost(postId);
+        Enroll enroll = enrollRepository.findByMemberIdAndPostId(memberId, postId).orElseThrow(() -> {
+            throw new IllegalArgumentException("참여하지 않은 구매글입니다.");
+        });
+        post.decreaseJoinCount();
+        enrollRepository.delete(enroll);
+    }
+}
