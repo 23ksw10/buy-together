6afa5e6b2030cc9fe3d8bdbdd8c7d0f5619d4481
Kim sun wook <kimsw9603@gmail.com>
2023-10-26 03:51:51 +0900

    10. PostCommentService로 전환

--- src/main/java/com/together/buytogether/postcomment/feature/RegisterComment.java
+++ src/main/java/com/together/buytogether/postcomment/feature/RegisterComment.java
@@ -1,30 +1,19 @@
 package com.together.buytogether.postcomment.feature;
 
-import com.together.buytogether.member.domain.Member;
-import com.together.buytogether.member.domain.MemberRepository;
 import com.together.buytogether.member.domain.SessionConst;
-import com.together.buytogether.post.domain.Post;
-import com.together.buytogether.post.domain.PostRepository;
-import com.together.buytogether.postcomment.domain.PostComment;
-import com.together.buytogether.postcomment.domain.PostCommentRepository;
 import com.together.buytogether.postcomment.dto.request.RegisterCommentDTO;
+import com.together.buytogether.postcomment.service.PostCommentService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RegisterComment {
-    MemberRepository memberRepository;
-    PostRepository postRepository;
-    PostCommentRepository postCommentRepository;
 
-    public RegisterComment(
-            MemberRepository memberRepository,
-            PostRepository postRepository,
-            PostCommentRepository postCommentRepository) {
-        this.memberRepository = memberRepository;
-        this.postRepository = postRepository;
-        this.postCommentRepository = postCommentRepository;
+    private final PostCommentService postCommentService;
+
+    public RegisterComment(PostCommentService postCommentService) {
+        this.postCommentService = postCommentService;
     }
 
     @PostMapping("/posts/{postId}/comments")
@@ -33,10 +22,7 @@ public class RegisterComment {
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId,
             @PathVariable Long postId,
             @RequestBody @Valid RegisterCommentDTO registerCommentDTO) {
-        Member member = memberRepository.getByMemberId(memberId);
-        Post post = postRepository.getByPostId(postId);
-        PostComment comment = registerCommentDTO.toDomain(member, post);
-        postCommentRepository.save(comment);
+        postCommentService.registerComment(memberId, postId, registerCommentDTO);
     }
 
 }
