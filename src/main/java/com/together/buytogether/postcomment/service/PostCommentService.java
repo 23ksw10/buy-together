33dea715a20ec4d5bf16d46bc615d472ad2182b5
Kim sun wook <kimsw9603@gmail.com>
2023-10-26 03:48:09 +0900

    8. create PostCommentService

---
+++ src/main/java/com/together/buytogether/postcomment/service/PostCommentService.java
@@ -0,0 +1,91 @@
+package com.together.buytogether.postcomment.service;
+
+import com.together.buytogether.common.service.CommonMemberService;
+import com.together.buytogether.common.service.CommonPostService;
+import com.together.buytogether.member.domain.Member;
+import com.together.buytogether.post.domain.Post;
+import com.together.buytogether.postcomment.domain.PostComment;
+import com.together.buytogether.postcomment.domain.PostCommentRepository;
+import com.together.buytogether.postcomment.dto.request.RegisterCommentDTO;
+import com.together.buytogether.postcomment.dto.request.UpdateCommentDTO;
+import com.together.buytogether.postcomment.dto.response.CommentResponseDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class PostCommentService {
+    private final PostCommentRepository postCommentRepository;
+    private final CommonPostService commonPostService;
+    private final CommonMemberService commonMemberService;
+
+    public PostCommentService(
+            PostCommentRepository postCommentRepository,
+            CommonPostService commonPostService,
+            CommonMemberService commonMemberService) {
+        this.postCommentRepository = postCommentRepository;
+        this.commonPostService = commonPostService;
+        this.commonMemberService = commonMemberService;
+    }
+
+    @Transactional
+    public void registerComment(Long memberId, Long postId, RegisterCommentDTO registerCommentDTO) {
+        Member member = commonMemberService.getMember(memberId);
+        Post post = commonPostService.getPost(postId);
+        PostComment postComment = registerCommentDTO.toDomain(member, post);
+        postCommentRepository.save(postComment);
+    }
+
+    @Transactional(readOnly = true)
+    public CommentResponseDTO getPostComment(Long commentId) {
+        PostComment postComment = postCommentRepository.getByCommentId(commentId);
+        return new CommentResponseDTO(
+                postComment.getCommentId(),
+                postComment.getPost().getPostId(),
+                postComment.getMember().getName(),
+                postComment.getContent(),
+                postComment.getCreatedAt().toString(),
+                postComment.getUpdatedAt().toString()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public List<CommentResponseDTO> getPostComments(Long postId) {
+        List<PostComment> comments = postCommentRepository.findAllByPostId(postId);
+        return comments.stream()
+                .map(c -> new CommentResponseDTO(
+                        c.getCommentId(),
+                        c.getPost().getPostId(),
+                        c.getMember().getName(),
+                        c.getContent(),
+                        c.getCreatedAt().toString(),
+                        c.getUpdatedAt().toString()
+                ))
+                .toList();
+    }
+
+    @Transactional
+    public void updateComment(Long memberId, Long commentId, UpdateCommentDTO updateCommentDTO) {
+        PostComment postComment = postCommentRepository.getByCommentId(commentId);
+        checkOwner(memberId, postComment);
+        postComment.update(
+                updateCommentDTO.content(),
+                LocalDateTime.now());
+    }
+
+    @Transactional
+    public void deleteComment(Long memberId, Long commentId) {
+        PostComment postComment = postCommentRepository.getByCommentId(commentId);
+        checkOwner(memberId, postComment);
+        postCommentRepository.delete(postComment);
+    }
+
+    public void checkOwner(Long memberId, PostComment postComment) {
+        if (!postComment.checkOwner(memberId)) {
+            throw new IllegalArgumentException("댓글 작성자가 아닙니다.");
+        }
+    }
+
+}
