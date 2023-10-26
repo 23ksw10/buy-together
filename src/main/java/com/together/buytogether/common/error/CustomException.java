c35c19f892904f779c12170d96cdd7d2b9feeba7
Kim sun wook <kimsw9603@gmail.com>
2023-10-27 04:07:13 +0900

    2. create CustomException

---
+++ src/main/java/com/together/buytogether/common/error/CustomException.java
@@ -0,0 +1,12 @@
+package com.together.buytogether.common.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+}
