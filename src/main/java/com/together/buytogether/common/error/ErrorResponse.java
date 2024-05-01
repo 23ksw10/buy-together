7e8aa4f59d8afd3b53a2c084b7a2a117b70c603f
Kim sun wook <kimsw9603@gmail.com>
2024-05-01 22:19:24 +0900

    13. add ErrorResponse

---
+++ src/main/java/com/together/buytogether/common/error/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.together.buytogether.common.error;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private final String error;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .error(errorCode.getHttpStatus().name())
+                        .message(errorCode.getMessage())
+                        .build()
+                );
+    }
+}
