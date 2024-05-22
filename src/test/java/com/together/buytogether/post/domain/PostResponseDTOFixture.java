8c06b6f77ee49d4aba907112ee5db8fbb2b7f302
Kim sun wook <kimsw9603@gmail.com>
2024-05-22 22:04:25 +0900

    13. add PostResponseDTOFixture

---
+++ src/test/java/com/together/buytogether/post/domain/PostResponseDTOFixture.java
@@ -0,0 +1,54 @@
+package com.together.buytogether.post.domain;
+
+import com.together.buytogether.post.dto.response.PostResponseDTO;
+
+import java.time.LocalDateTime;
+
+public class PostResponseDTOFixture {
+
+    String memberName = "작성자 이름";
+    Long postId = 1L;
+    String title = "제목";
+    String content = "내용";
+    String expiredAt = LocalDateTime.now().toString();
+
+    public static PostResponseDTOFixture aPostResponseDTO() {
+        return new PostResponseDTOFixture();
+    }
+
+    public PostResponseDTOFixture memberName(String memberName) {
+        this.memberName = memberName;
+        return this;
+    }
+
+    public PostResponseDTOFixture postId(Long postId) {
+        this.postId = postId;
+        return this;
+    }
+
+    public PostResponseDTOFixture title(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public PostResponseDTOFixture content(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public PostResponseDTOFixture expiredAt(String expiredAt) {
+        this.expiredAt = expiredAt;
+        return this;
+    }
+
+
+    public PostResponseDTO build() {
+        return PostResponseDTO.builder()
+                .memberName(memberName)
+                .postId(postId)
+                .title(title)
+                .content(content)
+                .expiredAt(expiredAt)
+                .build();
+    }
+}
