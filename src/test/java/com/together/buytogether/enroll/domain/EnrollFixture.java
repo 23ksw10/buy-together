3b5d106a9024f6c6027d6d3d4796345598de2c64
Kim sun wook <kimsw9603@gmail.com>
2024-05-23 14:27:44 +0900

    18. removed createdAt

--- src/test/java/com/together/buytogether/enroll/domain/EnrollFixture.java
+++ src/test/java/com/together/buytogether/enroll/domain/EnrollFixture.java
@@ -1,36 +1,29 @@
 package com.together.buytogether.enroll.domain;
 
+import com.together.buytogether.member.domain.Member;
 import com.together.buytogether.member.domain.MemberFixture;
+import com.together.buytogether.post.domain.Post;
 import com.together.buytogether.post.domain.PostFixture;
 
-import java.time.LocalDateTime;
-
 public class EnrollFixture {
-    private MemberFixture memberFixture = MemberFixture.aMember();
-    private PostFixture postFixture = PostFixture.aPost();
-
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private Member member = MemberFixture.aMember().build();
+    private Post post = PostFixture.aPost().build();
 
     public static EnrollFixture aEnroll() {
         return new EnrollFixture();
     }
 
-    public EnrollFixture memberFixture(MemberFixture memberFixture) {
-        this.memberFixture = memberFixture;
+    public EnrollFixture member(Member member) {
+        this.member = member;
         return this;
     }
 
-    public EnrollFixture postFixture(PostFixture postFixture) {
-        this.postFixture = postFixture;
-        return this;
-    }
-
-    public EnrollFixture createdAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
+    public EnrollFixture post(Post post) {
+        this.post = post;
         return this;
     }
 
     public Enroll build() {
-        return new Enroll(memberFixture.build(), postFixture.build(), createdAt);
+        return new Enroll(member, post);
     }
 }
