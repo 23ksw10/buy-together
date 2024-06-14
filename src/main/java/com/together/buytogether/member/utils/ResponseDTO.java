811bdcc0d9c03fcbcdbaf6c4a6705d38c77e9024
Kim sun wook <kimsw9603@gmail.com>
2024-06-14 22:36:28 +0900

    1. add ResponseDTO

---
+++ src/main/java/com/together/buytogether/member/utils/ResponseDTO.java
@@ -0,0 +1,33 @@
+package com.together.buytogether.member.utils;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ResponseDTO<T> {
+    private final Status status;
+    private final T data;
+    private final String message;
+
+    public static <T> ResponseDTO successResult(T data) {
+        return ResponseDTO.builder()
+                .status(Status.SUCCESS)
+                .data(data)
+                .message("Request Success")
+                .build();
+    }
+
+    public static <T> ResponseDTO failResult(T data) {
+        return ResponseDTO.builder()
+                .status(Status.FAIL)
+                .data(null)
+                .message("Request Fail")
+                .build();
+    }
+
+    private enum Status {
+        SUCCESS,
+        FAIL
+    }
+}
