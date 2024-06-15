43ca345366c1b33bec6e6c0b2c8386caf1e8953f
Kim sun wook <kimsw9603@gmail.com>
2024-06-16 00:23:44 +0900

    12. return ResponseEntity

--- src/main/java/com/together/buytogether/post/controller/PostController.java
+++ src/main/java/com/together/buytogether/post/controller/PostController.java
@@ -1,12 +1,15 @@
 package com.together.buytogether.post.controller;
 
+import com.together.buytogether.common.utils.ResponseDTO;
 import com.together.buytogether.member.domain.SessionConst;
 import com.together.buytogether.post.dto.request.RegisterPostDTO;
 import com.together.buytogether.post.dto.request.UpdatePostDTO;
 import com.together.buytogether.post.dto.response.PostResponseDTO;
+import com.together.buytogether.post.dto.response.UpdatePostResponseDTO;
 import com.together.buytogether.post.service.PostService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,32 +26,31 @@ public class PostController {
     }
 
     @PutMapping("/{postId}")
-    public void updatePost(@SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId,
-                           @PathVariable Long postId,
-                           @Valid @RequestBody UpdatePostDTO updatePostDTO) {
-        postService.updatePost(memberId, postId, updatePostDTO);
+    public ResponseEntity<ResponseDTO<UpdatePostResponseDTO>> updatePost(@SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId,
+                                                                         @PathVariable Long postId,
+                                                                         @Valid @RequestBody UpdatePostDTO updatePostDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.updatePost(memberId, postId, updatePostDTO));
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void registerPost(@RequestBody @Valid RegisterPostDTO registerPostDTO,
-                             @SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId) {
-        postService.registerPost(memberId, registerPostDTO);
+    public ResponseEntity<ResponseDTO<PostResponseDTO>> registerPost(@RequestBody @Valid RegisterPostDTO registerPostDTO,
+                                                                     @SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(postService.registerPost(memberId, registerPostDTO));
     }
 
     @GetMapping
-    public List<PostResponseDTO> getPosts() {
-        return postService.getPosts();
+    public ResponseEntity<ResponseDTO<List<PostResponseDTO>>> getPosts() {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.getPosts());
     }
 
     @GetMapping("/{postId}")
-    public PostResponseDTO getPost(@PathVariable Long postId) {
-        return postService.getPost(postId);
+    public ResponseEntity<ResponseDTO<PostResponseDTO>> getPost(@PathVariable Long postId) {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.getPost(postId));
     }
 
     @DeleteMapping("/{postId}")
-    public void deletePost(@SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId,
-                           @PathVariable Long postId) {
-        postService.deletePost(memberId, postId);
+    public ResponseEntity<ResponseDTO<String>> deletePost(@SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId,
+                                                          @PathVariable Long postId) {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.deletePost(memberId, postId));
     }
 }
