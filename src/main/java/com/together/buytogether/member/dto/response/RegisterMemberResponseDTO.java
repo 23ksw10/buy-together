dc81a79d6b0dbd5fb900af6968201e68b5ae121c
Kim sun wook <kimsw9603@gmail.com>
2024-06-16 00:37:03 +0900

    16. removed @NotBlank and add Assert.hasText

--- src/main/java/com/together/buytogether/member/dto/response/RegisterMemberResponseDTO.java
+++ src/main/java/com/together/buytogether/member/dto/response/RegisterMemberResponseDTO.java
@@ -1,16 +1,18 @@
 package com.together.buytogether.member.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
+import org.springframework.util.Assert;
 
 
 @Builder
 public record RegisterMemberResponseDTO(
-        @NotBlank(message = "이름은 필수 값입니다")
         String name,
-        @NotBlank(message = "로그인 아이디는 필수 값입니다")
         String email,
-        @NotBlank(message = "전화번호는 필수 값입니다")
         String phoneNumber
 ) {
+    public RegisterMemberResponseDTO {
+        Assert.hasText(name, "이름은 필수 값입니다.");
+        Assert.hasText(email, "이메일은 필수 값입니다.");
+        Assert.hasText(phoneNumber, "전화번호는 필수 값입니다.");
+    }
 }
