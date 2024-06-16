b55d5986cc2f293cde31793e18f9c08eaf56170a
Kim sun wook <kimsw9603@gmail.com>
2024-06-17 01:52:46 +0900

    30. 이름 변경 및 CustomException 사용

--- src/test/java/com/together/buytogether/postcomment/domain/PostCommentDomainTest.java
+++ src/test/java/com/together/buytogether/postcomment/domain/PostCommentDomainTest.java
@@ -1,7 +1,9 @@
 package com.together.buytogether.postcomment.domain;
 
+import com.together.buytogether.common.error.CustomException;
 import com.together.buytogether.post.domain.PostFixture;
 import com.together.buytogether.post.domain.PostStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,29 +12,34 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class PostCommentTest {
+public class PostCommentDomainTest {
+    private String updateCommentContent;
+
+    @BeforeEach
+    void setUp() {
+        updateCommentContent = "Updated Content";
+    }
+
     @Test
     @DisplayName("댓글 수정")
     void updateComment() {
         PostComment postComment = PostCommentFixture.aPostComment().build();
-        String beforeContent = postComment.getContent();
         LocalDateTime updateDate = LocalDateTime.now();
-        postComment.update("댓글 수정", updateDate);
-        assertThat(beforeContent).isEqualTo("댓글 내용");
-        assertThat(postComment.getContent()).isEqualTo("댓글 수정");
+        postComment.update(updateCommentContent, updateDate);
+        assertThat(postComment.getContent()).isEqualTo(updateCommentContent);
         assertThat(postComment.getUpdatedAt()).isEqualTo(updateDate);
     }
 
     @Test
-    @DisplayName("댓글 수정 - 게시글 상태가 CLOSED일 경우 예외가 발생한다")
-    void fail_invalid_status_post_status_updateComment() {
+    @DisplayName("댓글 수정 - 게시글 상태가 CLOSED 경우 예외가 발생한다")
+    void throwExceptionWhenUpdateCommentOnClosedPost() {
         PostFixture postFixture = PostFixture.aPost().status(PostStatus.CLOSED);
         PostComment postComment = PostCommentFixture.aPostComment().post(postFixture.build()).build();
         LocalDateTime updateDate = LocalDateTime.now();
         assertThatThrownBy(() -> {
-            postComment.update("댓글 수정", updateDate);
+            postComment.update(updateCommentContent, updateDate);
         })
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessage("종료된 게시글 댓글은 수정할 수 없습니다.");
+                .isInstanceOf(CustomException.class)
+                .hasMessage("이미 마감된 게시글입니다");
     }
 }
