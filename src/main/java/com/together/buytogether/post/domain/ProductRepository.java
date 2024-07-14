8e8a0352f5acd05985ce0fe74a6cdad8ef89069c
Kim sun wook <kimsw9603@gmail.com>
2024-07-14 21:07:41 +0900

    8. add ProductRepository

---
+++ src/main/java/com/together/buytogether/post/domain/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.together.buytogether.post.domain;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.together.buytogether.common.error.CustomException;
+import com.together.buytogether.common.error.ErrorCode;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	@Query("SELECT p FROM Product p JOIN FETCH p.post post JOIN FETCH post.member WHERE p.productId = :productId")
+	Optional<Product> findById(@Param("productId") Long productId);
+
+	default Product getByProductId(Long postId) {
+		return findById(postId).orElseThrow(
+			() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND)
+		);
+	}
+}
