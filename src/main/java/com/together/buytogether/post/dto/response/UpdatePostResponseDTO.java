7acabbe23351711e97584645e770b4b61f6efc30
Kim sun wook <kimsw9603@gmail.com>
2024-07-14 22:18:53 +0900

    15. remove joinCount, maxCount

--- src/main/java/com/together/buytogether/post/dto/response/UpdatePostResponseDTO.java
+++ src/main/java/com/together/buytogether/post/dto/response/UpdatePostResponseDTO.java
@@ -1,25 +1,24 @@
 package com.together.buytogether.post.dto.response;
 
-import lombok.Builder;
+import java.time.LocalDateTime;
+
 import org.springframework.util.Assert;
 
-import java.time.LocalDateTime;
+import lombok.Builder;
 
 @Builder
 public record UpdatePostResponseDTO(
-        Long postId,
-        String title,
-        String content,
-        Long joinCount,
-        String memberName,
-        LocalDateTime updatedAt
+	Long postId,
+	String title,
+	String content,
+	String memberName,
+	LocalDateTime updatedAt
 ) {
-    public UpdatePostResponseDTO {
-        Assert.notNull(postId, "게시글 ID는 필수 입니다");
-        Assert.hasText(title, "제목은 필수 입니다");
-        Assert.hasText(content, "내용은 필수 입니다");
-        Assert.isTrue(joinCount >= 0, "참여 인원 수는 0 이상이어야 합니다");
-        Assert.hasText(memberName, "판매자 이름은 필수 입니다");
-        Assert.notNull(updatedAt, "업데이트 시간은 null일 수 없습니다");
-    }
+	public UpdatePostResponseDTO {
+		Assert.notNull(postId, "게시글 ID는 필수 입니다");
+		Assert.hasText(title, "제목은 필수 입니다");
+		Assert.hasText(content, "내용은 필수 입니다");
+		Assert.hasText(memberName, "판매자 이름은 필수 입니다");
+		Assert.notNull(updatedAt, "업데이트 시간은 null일 수 없습니다");
+	}
 }
