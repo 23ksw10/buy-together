d7391a9496982c4fe50ff1d42e78ca63298d7fb4
Kim sun wook <kimsw9603@gmail.com>
2024-07-14 21:18:02 +0900

    12. remove joinCount field, maxJoinCount field

--- src/test/java/com/together/buytogether/post/domain/RegisterPostDTOFixture.java
+++ src/test/java/com/together/buytogether/post/domain/RegisterPostDTOFixture.java
@@ -1,61 +1,45 @@
 package com.together.buytogether.post.domain;
 
-import com.together.buytogether.post.dto.request.RegisterPostDTO;
-
 import java.time.LocalDateTime;
 
+import com.together.buytogether.post.dto.request.RegisterPostDTO;
+
 public class RegisterPostDTOFixture {
-    private String title = "title";
-    private String content = "content";
+	private String title = "title";
+	private String content = "content";
+	private PostStatus status = PostStatus.OPEN;
+	private LocalDateTime expiredAt = LocalDateTime.now();
 
-    private Long joinCount = 0L;
-    private Long maxJoinCount = 100L;
+	public static RegisterPostDTOFixture aRegisterPostDTO() {
+		return new RegisterPostDTOFixture();
+	}
 
-    private PostStatus satus = PostStatus.OPEN;
-    private LocalDateTime expiredAt = LocalDateTime.now();
+	public RegisterPostDTOFixture title(String title) {
+		this.title = title;
+		return this;
+	}
 
-    public static RegisterPostDTOFixture aRegisterPostDTO() {
-        return new RegisterPostDTOFixture();
-    }
+	public RegisterPostDTOFixture content(String content) {
+		this.content = content;
+		return this;
+	}
 
-    public RegisterPostDTOFixture title(String title) {
-        this.title = title;
-        return this;
-    }
+	public RegisterPostDTOFixture expiredAt(LocalDateTime expiredAt) {
+		this.expiredAt = expiredAt;
+		return this;
+	}
 
-    public RegisterPostDTOFixture content(String content) {
-        this.content = content;
-        return this;
-    }
+	public RegisterPostDTOFixture status(PostStatus status) {
+		this.status = status;
+		return this;
+	}
 
-    public RegisterPostDTOFixture maxJoinCount(Long maxJoinCount) {
-        this.maxJoinCount = maxJoinCount;
-        return this;
-    }
-
-    public RegisterPostDTOFixture expiredAt(LocalDateTime expiredAt) {
-        this.expiredAt = expiredAt;
-        return this;
-    }
-
-    public RegisterPostDTOFixture joinCount(Long joinCount) {
-        this.joinCount = joinCount;
-        return this;
-    }
-
-    public RegisterPostDTOFixture status(PostStatus status) {
-        this.satus = status;
-        return this;
-    }
-
-    public RegisterPostDTO build() {
-        return RegisterPostDTO.builder()
-                .title(title)
-                .content(content)
-                .status(satus)
-                .joinCount(joinCount)
-                .maxJoinCount(maxJoinCount)
-                .expiredAt(expiredAt)
-                .build();
-    }
+	public RegisterPostDTO build() {
+		return RegisterPostDTO.builder()
+			.title(title)
+			.content(content)
+			.status(status)
+			.expiredAt(expiredAt)
+			.build();
+	}
 }
