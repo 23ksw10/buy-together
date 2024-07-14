8e711b5e302ee327c868449c2d688b52a743fb2b
Kim sun wook <kimsw9603@gmail.com>
2024-07-14 23:53:58 +0900

    22. ProductRepository 추가

--- src/test/java/com/together/buytogether/enroll/repository/EnrollRepositoryTest.java
+++ src/test/java/com/together/buytogether/enroll/repository/EnrollRepositoryTest.java
@@ -1,12 +1,11 @@
 package com.together.buytogether.enroll.repository;
 
-import com.together.buytogether.config.JpaAuditingConfig;
-import com.together.buytogether.enroll.domain.Enroll;
-import com.together.buytogether.enroll.domain.EnrollRepository;
-import com.together.buytogether.member.domain.Member;
-import com.together.buytogether.member.domain.MemberRepository;
-import com.together.buytogether.post.domain.Post;
-import com.together.buytogether.post.domain.PostRepository;
+import static com.together.buytogether.enroll.domain.EnrollFixture.*;
+import static com.together.buytogether.member.domain.MemberFixture.*;
+import static com.together.buytogether.post.domain.PostFixture.*;
+import static com.together.buytogether.post.domain.ProductFixture.*;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,10 +14,15 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-import static com.together.buytogether.enroll.domain.EnrollFixture.aEnroll;
-import static com.together.buytogether.member.domain.MemberFixture.aMember;
-import static com.together.buytogether.post.domain.PostFixture.aPost;
-import static org.assertj.core.api.Assertions.assertThat;
+import com.together.buytogether.config.JpaAuditingConfig;
+import com.together.buytogether.enroll.domain.Enroll;
+import com.together.buytogether.enroll.domain.EnrollRepository;
+import com.together.buytogether.member.domain.Member;
+import com.together.buytogether.member.domain.MemberRepository;
+import com.together.buytogether.post.domain.Post;
+import com.together.buytogether.post.domain.PostRepository;
+import com.together.buytogether.post.domain.Product;
+import com.together.buytogether.post.domain.ProductRepository;
 
 @DisplayName("Enroll JPA 연결 테스트")
 @DataJpaTest
@@ -26,72 +30,69 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class EnrollRepositoryTest {
 
-    @Autowired
-    PostRepository postRepository;
+	Member member;
+	Post post;
+	Member savedMember;
+	Post savedPost;
+	Product product;
+	Enroll enroll;
+	@Autowired
+	private PostRepository postRepository;
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private EnrollRepository enrollRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    @Autowired
-    MemberRepository memberRepository;
+	@BeforeEach
+	void setUp() {
+		member = aMember().build();
+		savedMember = memberRepository.save(member);
+		post = aPost().member(member).build();
+		savedPost = postRepository.save(post);
+		product = productRepository.save(aProduct().post(savedPost).build());
+		enroll = aEnroll().member(member).product(product).build();
+	}
 
-    @Autowired
-    EnrollRepository enrollRepository;
+	@Test
+	@DisplayName("insert 테스트")
+	public void insertEnroll() {
+		// when
+		Enroll savedEnroll = enrollRepository.saveAndFlush(enroll);
 
-    Member member;
-    Post post;
-    Member savedMember;
-    Post savedPost;
+		// then
+		assertThat(enrollRepository.count()).isEqualTo(1);
+		assertThat(savedEnroll.getEnrollId()).isNotNull();
+		assertThat(savedEnroll.getMember()).isEqualTo(member);
+		assertThat(savedEnroll.getProduct()).isEqualTo(product);
+	}
 
-    Enroll enroll;
+	@Test
+	@DisplayName("delete 테스트")
+	public void deleteEnroll() {
 
-    @BeforeEach
-    void setUp() {
-        member = aMember().build();
-        savedMember = memberRepository.save(member);
-        post = aPost().member(member).build();
-        savedPost = postRepository.save(post);
-        enroll = aEnroll().member(member).post(post).build();
-    }
+		Enroll savedEnroll = enrollRepository.save(enroll);
+		// when
+		enrollRepository.delete(savedEnroll);
 
-    @Test
-    @DisplayName("insert 테스트")
-    public void insertEnroll() {
+		// then
+		assertThat(enrollRepository.count()).isEqualTo(0);
 
-        // when
-        Enroll savedEnroll = enrollRepository.save(enroll);
+	}
 
-        // then
-        assertThat(enrollRepository.count()).isEqualTo(1);
-        assertThat(savedEnroll.getEnrollId()).isNotNull();
-        assertThat(savedEnroll.getMember()).isEqualTo(member);
-        assertThat(savedEnroll.getPost()).isEqualTo(post);
-    }
+	@Test
+	@DisplayName("select 테스트")
+	public void selectEnroll() {
 
-    @Test
-    @DisplayName("delete 테스트")
-    public void deleteEnroll() {
+		Enroll savedEnroll = enrollRepository.saveAndFlush(enroll);
 
-        Enroll savedEnroll = enrollRepository.save(enroll);
-        // when
-        enrollRepository.delete(savedEnroll);
+		// when
+		Enroll foundEnroll = enrollRepository.getEnroll(savedEnroll.getEnrollId());
 
-        // then
-        assertThat(enrollRepository.count()).isEqualTo(0);
-
-    }
-
-    @Test
-    @DisplayName("select 테스트")
-    public void selectEnroll() {
-
-        enrollRepository.saveAndFlush(enroll);
-
-        // when
-        Enroll foundEnroll = enrollRepository.getEnroll(savedMember.getMemberId(), savedPost.getPostId());
-
-        // then
-        assertThat(foundEnroll).isNotNull();
-        assertThat(foundEnroll.getMember()).isEqualTo(savedMember);
-        assertThat(foundEnroll.getPost()).isEqualTo(savedPost);
-
-    }
-
+		// then
+		assertThat(foundEnroll).isNotNull();
+		assertThat(foundEnroll.getMember()).isEqualTo(savedMember);
+		assertThat(foundEnroll.getProduct()).isEqualTo(product);
+	}
 }
