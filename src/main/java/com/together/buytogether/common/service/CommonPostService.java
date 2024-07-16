48cb2bf2101b4547fa14034189ae8ffe7b7584ac
Kim sun wook <kimsw9603@gmail.com>
2024-07-16 19:11:17 +0900

    3. getPost method cache 적용

--- src/main/java/com/together/buytogether/common/service/CommonPostService.java
+++ src/main/java/com/together/buytogether/common/service/CommonPostService.java
@@ -1,18 +1,21 @@
 package com.together.buytogether.common.service;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
 import com.together.buytogether.post.domain.Post;
 import com.together.buytogether.post.domain.PostRepository;
-import org.springframework.stereotype.Service;
 
 @Service
 public class CommonPostService {
-    private final PostRepository postRepository;
+	private final PostRepository postRepository;
 
-    public CommonPostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
+	public CommonPostService(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
 
-    public Post getPost(Long postId) {
-        return postRepository.getByPostId(postId);
-    }
+	@Cacheable(key = "#postId", value = "post")
+	public Post getPost(Long postId) {
+		return postRepository.getByPostId(postId);
+	}
 }
